c73fcf3b9f23d7ef41e030acc3ee8420fd38bd19
Marc Moten <marcmotenii@gmail.com>
2021-08-10 19:26:01 -0500

    index.js now exports model relationships

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 const User = require("./User");
 const Collection = require("./Collection");
 const Item = require("./Item");
+const { Model } = require("sequelize/types");
 
 User.hasMany(Collection, {
     foreignKey: 'user_id',
@@ -17,3 +18,5 @@ Collection.hasMany(Item, {
 Item.belongsTo(Collection, {
     foreignKey: 'collection_id',
 });
+
+module.exports = { User, Collection, Item }
