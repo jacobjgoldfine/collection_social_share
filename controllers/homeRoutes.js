7f4363fa7966037b01243f0083a0115cf0462e6e
Dominque Jenkins <dominquejenkins14@gmail.com>
2021-08-14 11:20:54 -0500

    added collection item and user routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,12 +1,105 @@
 const router = require('express').Router();
+const { Collection, Item, User } = require('../models');
+const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
-  // Send the rendered Handlebars.js template back as the response
-  res.render('homepage');
+  try {
+    // Get all projects and JOIN with user data
+    const collectionData = await Collection.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    // Serialize data so the template can read it
+    const collections = collectionData.map((collection) => collection.get({ plain: true }));
+
+    // Pass serialized data and session flag into template
+    res.render('homepage', { 
+      collections, 
+      logged_in: req.session.logged_in 
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/login', async (req, res) => {
-  res.render('login')
+router.get('/collection/:id', async (req, res) => {
+  try {
+    const collectionData = await Collection.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    const collection = collectionData.get({ plain: true });
+
+    res.render('collection', {
+      ...collection,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
+
+router.get('/item/:id', async (req, res) => {
+  try {
+    const itemData = await Item.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    const item = itemData.get({ plain: true });
+
+    res.render('item', {
+      ...item,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Use withAuth middleware to prevent access to route
+router.get('/profile', withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: collection  }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render('profile', {
+      ...user,
+      logged_in: true
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/login', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect('/profile');
+    return;
+  }
+
+  res.render('login');
+});
+
 
 module.exports = router;
