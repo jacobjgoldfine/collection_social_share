c7073f5ea974f05e791dc995b37a4247a12f6d2f
Syed <82619273+Syedm-hub@users.noreply.github.com>
2021-08-14 08:09:27 -0700

    updating index.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,13 +1,4 @@
 const router = require('express').Router();
-<<<<<<< HEAD
-
-const apiRoutes = require('./api');
-const homeRoutes = require('./home-routes');
-
-router.use('/', homeRoutes);
-router.use('/api', apiRoutes);
-
-=======
 const collectionsRoutes = require("./collectionRoutes")
 const itemRoutes = require('./itemRoutes');
 const userRoutes = require('./userRoutes');
@@ -15,6 +6,5 @@ const userRoutes = require('./userRoutes');
 router.use('/collections', collectionsRoutes);
 router.use('/items', itemRoutes);
 router.use('/users', userRoutes);
->>>>>>> 22592ff40737d6d93315dc3fd94ca295b6219763
 
 module.exports = router;
