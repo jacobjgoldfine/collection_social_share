2fe37c750961360a7ed6482e5643946feef17705
Dominque Jenkins <dominquejenkins14@gmail.com>
2021-08-15 11:41:44 -0500

    updated routes and added a readme file

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 
-const collectionsRoutes = require("./collectionRoutes")
+const collectionRoutes = require("./collectionRoutes")
 const itemRoutes = require('./itemRoutes');
 const userRoutes = require('./userRoutes');
 
-router.use('/collections', collectionsRoutes);
-router.use('/items', itemRoutes);
-router.use('/users', userRoutes);
+router.use('/collection', collectionRoutes);
+router.use('/item', itemRoutes);
+router.use('/user', userRoutes);
 
 module.exports = router;
--- controllers/api/itemRoutes.js
+++ controllers/api/itemRoutes.js
@@ -1,5 +1,39 @@
 const router = require('express').Router();
 const { Item } = require('../../models');
 
+router.post("/", async (req, res) => {
+    try {
+      const newItem = await Item.create({
+        ...req.body,
+        user_id: req.session.user_id,
+      });
+  
+      res.status(200).json(newItem);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
+
+  
+  
+router.delete("/:id", async (req, res) => {
+    try {
+        const itemData = await Item.destroy({
+        where: {
+            id: req.params.id,
+            user_id: req.session.user_id,
+        },
+    });
+  
+    if (!itemData) {
+        res.status(404).json({ message: "No item found with this id!" });
+        return;
+    }
+  
+    res.status(200).json(itemData);
+        } catch (err) {
+    res.status(500).json(err);
+    }
+});
 
 module.exports = router;
