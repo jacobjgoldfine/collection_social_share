206f56c54b97dbaa83318bb7a7acea4c4fc84318
Jacob Goldfine <jacobjgoldfine@gmail.com>
2021-08-17 18:41:37 -0500

    fixed collection and item page viewing

--- controllers/api/collectionRoutes.js
+++ controllers/api/collectionRoutes.js
@@ -1,45 +1,44 @@
 const router = require("express").Router();
-const { Collection, User, Item } = require("../../models");
+const { Collection } = require("../../models");
 
+// router.get("/", (req, res) => {
+//   Collection.findAll().then((collectionData) => {
+//     res.json(collectionData);
+//   });
+// });
 
-router.get('/', (req, res) => {
-  Collection.findAll().then((collectionData) => {
-    res.json(collectionData);
-  });
-});
-
-router.post('/', (req, res) => {
-  Collection.create( req.body)
+router.post("/", (req, res) => {
+  Collection.create(req.body)
     .then((newCollection) => {
       res.json(newCollection);
     })
     .catch((err) => {
       res.json(err);
     });
-  });
-
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
-  Collection.update(
-    {col_name: req.body.col_name,
-    col_description: req.body.col_description},
-    {
-      where: {id: req.params.id}
-    })
-    .then(collectionData => {
-      if (!collectionData) {
-        res.status(404).json({ message: 'No Collection found with that ID.' });
-        return;
-      }
-      res.json(collectionData);
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
 });
 
-router.delete('/:id', async (req, res) => {
+// router.put('/:id', (req, res) => {
+//   // update a category by its `id` value
+//   Collection.update(
+//     {col_name: req.body.col_name,
+//     col_description: req.body.col_description},
+//     {
+//       where: {id: req.params.id}
+//     })
+//     .then(collectionData => {
+//       if (!collectionData) {
+//         res.status(404).json({ message: 'No Collection found with that ID.' });
+//         return;
+//       }
+//       res.json(collectionData);
+//     })
+//     .catch(err => {
+//       console.log(err);
+//       res.status(500).json(err);
+//     });
+// });
+
+router.delete("/:id", async (req, res) => {
   try {
     const collectionData = await Collection.destroy({
       where: {
