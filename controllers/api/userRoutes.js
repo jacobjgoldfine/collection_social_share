146f8918fc7ef196ba48692c9e9768800e8971d6
Marc Moten <marcmotenii@gmail.com>
2021-08-17 16:27:48 -0500

    added create user post route back

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -16,7 +16,26 @@ const { User } = require('../../models');
       res.status(400).json(err);
     }
   });*/
-  
+  // CREATE new user
+router.post('/', async (req, res) => {
+  try {
+    const dbUserData = await User.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    });
+
+    req.session.save(() => {
+      req.session.loggedIn = true;
+
+      res.status(200).json(dbUserData);
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
   router.post('/login', async (req, res) => {
     try {
       const userData = await User.findOne({ where: { email: req.body.email } });
