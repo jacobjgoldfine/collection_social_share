82dd5e57795badda522379b88c9fef505b457e98
Johnny Dickerson Jr <lilman.jdick@gmail.com>
2021-08-17 21:13:23 -0500

    added split url for cropping images on page

--- utils/helpers.js
+++ utils/helpers.js
@@ -7,4 +7,12 @@ module.exports = {
       new Date(date).getFullYear() + 5
     }`;
   },
+// split url to add cropping to image
+  split_url: (url) => {
+    let str = url
+    let splitString = str.split("upload");
+    return splitString[0] + "upload/c_thumb,w_200,g_face" + splitString[1];
+  }
 };
+
+
