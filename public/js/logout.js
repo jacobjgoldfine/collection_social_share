555b2ed8b0f537b8369bc19b502d121745f3f392
Mamii868 <83027431+Mamii868@users.noreply.github.com>
2021-08-19 00:20:10 -0500

    Revert "Login/logout final"

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,10 +1,11 @@
 const logout = async () => {
-  const response = await fetch('/api/user/logout', {
+  const response = await fetch('/api/users/logout', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
   });
 
   if (response.ok) {
+    window.alert("logging out,")
     document.location.replace('/login');
   } else {
     alert(statusText);
